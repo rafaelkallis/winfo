cb257ff0f88c173683ba0a41e370ef6f7c1c8881
rafael <rafael@rafaelkallis.com>
2015-11-29 14:50:05 +0100

    Added New fields

--- HA6/src/main/java/we/are/awesome/TodoDAO.java
+++ HA6/src/main/java/we/are/awesome/TodoDAO.java
@@ -2,13 +2,31 @@ package we.are.awesome;
 
 import java.io.Serializable;
 
+import org.camunda.bpm.engine.identity.User;
+
 public class TodoDAO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	private final String title;
 	private final String description;
+	private final Boolean isDone;
+	private final User assignedUser;
 	
+	public TodoDAO(){
+		this.title 			= null;
+		this.description 	= null;
+		this.isDone 		= null;
+		this.assignedUser 	= null;
+	}
+	
+	public TodoDAO(String title, String description,Boolean isDone, User assignedUser){
+		this.title 			= title;
+		this.description 	= description;
+		this.isDone 		= isDone;
+		this.assignedUser 	= assignedUser;
+	}
+
 	public String getTitle() {
 		return title;
 	}
@@ -17,14 +35,11 @@ public class TodoDAO implements Serializable{
 		return description;
 	}
 
-	
-	public TodoDAO(){
-		this.title = null;
-		this.description = null;
+	public Boolean getIsDone() {
+		return isDone;
 	}
-	
-	public TodoDAO(String title, String description){
-		this.title = title;
-		this.description = description;
+
+	public User getAssignedUser() {
+		return assignedUser;
 	}
 }
--- HA6/src/main/java/we/are/awesome/TodoEntity.java
+++ HA6/src/main/java/we/are/awesome/TodoEntity.java
@@ -2,6 +2,9 @@ package we.are.awesome;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+
+import org.camunda.bpm.engine.identity.User;
+
 import java.io.Serializable;
 
 @Entity
@@ -11,41 +14,65 @@ public class TodoEntity implements Serializable {
 
 	@Id
 	@GeneratedValue
-	protected Long id;
+	protected Long 		id;
 	
-	protected String title;	  
+	protected String 	title;	  
 
-	protected String description;
+	protected String 	description;
 	
-	protected Boolean isDone;
+	protected Boolean 	isDone;
+	
+	protected User		assignedUser;
 	  
-	public TodoEntity(String title, String description){
-		this.title = title;
-		this.description = description;
+	public TodoEntity(String title, String description,User assignedUser){
+		this.title 			= title;
+		this.description 	= description;
+		this.isDone 		= false;
+		this.assignedUser 	= assignedUser;
 	}
 	  
 	public TodoEntity(){
 		  
 	}
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
-	public void setDescription(String description) {
-		this.description = description;
-	}
-  
-	public String getTitle() {
-	  return title;
-	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
-	public Long getId(){
-		return id;
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getIsDone() {
+		return isDone;
+	}
+
+	public void setIsDone(Boolean isDone) {
+		this.isDone = isDone;
+	}
+
+	public User getAssignedUser() {
+		return assignedUser;
+	}
+
+	public void setAssignedUser(User assignedUser) {
+		this.assignedUser = assignedUser;
 	}
 }
 
