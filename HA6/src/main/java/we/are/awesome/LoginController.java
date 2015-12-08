8682e13dcea943f8fea4b2fe9be09c23c670d767
rafael <rafael@rafaelkallis.com>
2015-12-08 10:36:37 +0100

    register working

--- HA6/src/main/java/we/are/awesome/LoginController.java
+++ HA6/src/main/java/we/are/awesome/LoginController.java
@@ -3,8 +3,13 @@ package we.are.awesome;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
 import javax.persistence.Query;
 
+@Stateless
+@Named
 public class LoginController extends Controller { 
 	
 	private Boolean isProjektleiter;
