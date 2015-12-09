576823068366726253f7fc432468ac24df7de351
rafael <rafael@rafaelkallis.com>
2015-12-09 21:10:23 +0100

    hotfix for Long casting issue

--- HA6/src/main/java/we/are/awesome/TaskBearbeitenController.java
+++ HA6/src/main/java/we/are/awesome/TaskBearbeitenController.java
@@ -7,8 +7,12 @@ import javax.inject.Named;
 @Stateless
 public class TaskBearbeitenController extends Controller{
 	
+	public void submit(){
+		super.completeTask();
+	}
+	
 	public TaskDAO getTaskDAO(){
-		Long taskId = super.businessProcess.getVariable("taskId");
+		Long taskId = Long.parseLong(super.businessProcess.getVariable("taskId").toString()); //FIXME: getVariable() returns String instead of Long when Task was made by Mitarbeiter
 		return new TaskDAO(super.getTaskEntity(taskId));
 	}
 }
