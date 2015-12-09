9fe17f30380afac2710f5f3620f27ae0a45d1737
rafael <rafael@rafaelkallis.com>
2015-12-09 21:10:39 +0100

    clean up

--- HA6/src/main/java/we/are/awesome/PersonAuswaehlenController.java
+++ HA6/src/main/java/we/are/awesome/PersonAuswaehlenController.java
@@ -6,10 +6,15 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 @Named
 @Stateless
 public class PersonAuswaehlenController extends Controller{
+	
+	public void submit(){
+		super.completeTask();
+	}
 	
 	public TaskDAO getTaskDAO(){
 		Long taskId = super.businessProcess.getVariable("taskId");
@@ -19,7 +24,7 @@ public class PersonAuswaehlenController extends Controller{
 	public List<UserDAO> getMitarbeiterUserList(){
 		List<UserDAO> userList = new ArrayList<UserDAO>();	
 
-		Query query = super.entityManager.createQuery("SELECT u FROM UserEntity u WHERE u.grouppe=we.are.awesome.Grouppe.Mitarbeiter");
+		TypedQuery<UserEntity> query = super.entityManager.createQuery("SELECT u FROM UserEntity u WHERE u.isProjektleiter = FALSE",UserEntity.class);
 		List<UserEntity> rs = query.getResultList();
 		for(UserEntity userEntity : rs){
 			userList.add(new UserDAO(userEntity));		
@@ -27,13 +32,14 @@ public class PersonAuswaehlenController extends Controller{
 		return userList;
 	}
 	
-	public Integer getNAssignedTasks(){		
-		Long userId = super.businessProcess.getVariable("selectedUserId");
-		if(userId == null){
-			return 0;
-		}else{
-			Query query = super.entityManager.createQuery("SELECT COUNT(*) FROM TaskEntity WHERE assignedUserId='"+userId+"'");
-			return query.getFirstResult();
+	public Long getNAssignedTasks(){		
+		Long userId = null;
+		Long NTasks = 0L;
+		if((userId = super.businessProcess.getVariable("selectedUserId")) != null){
+			Query query = super.entityManager.createQuery("SELECT COUNT(*) FROM TaskEntity t WHERE t.assignedUserId = :userId").setParameter("userId", userId);
+			NTasks = (Long) query.getSingleResult();
 		}
+		System.out.println("Size of NAssignedTasks = "+NTasks);
+		return NTasks;
 	}
 }
