7a4484334611b4f9e2fcab88044f9e95e5ac2271
rafael <rafael@rafaelkallis.com>
2015-12-09 23:02:15 +0100

    Added Janitor for sanitizeing during looping in workspaces

---
+++ HA6/src/main/java/we/are/awesome/Janitor.java
@@ -0,0 +1,25 @@
+package we.are.awesome;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+
+@Named
+@Stateless
+public class Janitor {
+	
+	private static final String[] PROJEKTLEITER_WORKSPACE_PROCESS_VARIABLES = {"taskId","title","description","isFreierTask","assignedUserId"};
+	private static final String[] MITARBEITER_WORKSPACE_PROCESS_VARIABLES = {"taskId","title","description"}; 
+	
+	public void sanitizeProjektleiterProcessVariables(DelegateExecution execution){
+		for(String processVariable : Janitor.PROJEKTLEITER_WORKSPACE_PROCESS_VARIABLES){
+			execution.removeVariable(processVariable);
+		}
+	}
+	
+	public void sanitizeMitarbeiterProcessVariables(DelegateExecution execution){
+		for(String processVariable : Janitor.MITARBEITER_WORKSPACE_PROCESS_VARIABLES){
+			execution.removeVariable(processVariable);
+		}
+	}
+}
