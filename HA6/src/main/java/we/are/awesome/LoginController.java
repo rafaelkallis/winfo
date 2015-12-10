6b454414dde7fdbfdfdd717ac1a34a36583df6ac
Rafael <rafael@rafaelkallis.com>
2015-12-10 15:20:31 +0100

    Updated Login

--- HA6/src/main/java/we/are/awesome/LoginController.java
+++ HA6/src/main/java/we/are/awesome/LoginController.java
@@ -2,7 +2,6 @@ package we.are.awesome;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.TypedQuery;
@@ -12,36 +11,27 @@ import javax.persistence.TypedQuery;
 public class LoginController extends Controller { 
 		
 	public void login(){
+
 		super.completeProcessInstanceForm();
 	}
 	
-	public void register(){
-		String userName 		= super.businessProcess.getVariable("userName");
-		Boolean isProjektleiter = super.businessProcess.getVariable("isProjektleiter");
-		
-		UserEntity newUser 		= new UserEntity(userName,isProjektleiter);
-		
+	public void register(String userName, Boolean isProjektleiter){
+		 		
+		UserEntity newUser 		= new UserEntity(userName,isProjektleiter);	
 		super.entityManager.persist(newUser);
-		
-		super.businessProcess.setVariable("loggedUserId", newUser.getId());
-		
-		super.entityManager.flush();
-				
+		super.businessProcess.setVariable("loggedUserId", newUser.getId());	
+		super.entityManager.flush();		
 		this.login();
 	}
 	
 	public List<UserDAO> getUsers(){
-		List<UserDAO> userList = new ArrayList<UserDAO>();	
 
+		List<UserDAO> userList = new ArrayList<UserDAO>();	
 		TypedQuery<UserEntity> query = super.entityManager.createQuery("SELECT u FROM UserEntity u",UserEntity.class);
 		List<UserEntity> rs = query.getResultList();
 		for(UserEntity userEntity : rs){
 			userList.add(new UserDAO(userEntity));		
 		}
 		return userList;
-	}
-	
-	public Boolean getIsProjektleiter(Long loggedUserId){
-		return super.getUserEntity(loggedUserId).getIsProjektleiter();
 	}
 }
