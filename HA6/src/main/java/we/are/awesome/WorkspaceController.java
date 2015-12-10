26d71b514e333ca01cd9db8dc1da98b7d0a8455c
rafael <rafael@rafaelkallis.com>
2015-12-10 17:01:34 +0100

    deleted Workspace Controller

--- HA6/src/main/java/we/are/awesome/WorkspaceController.java
+++
@@ -1,8 +0,0 @@
-package we.are.awesome;
-
-public abstract class WorkspaceController extends Controller {
-
-	public String getAction(){
-		return super.businessProcess.getVariable("action");
-	}
-}
