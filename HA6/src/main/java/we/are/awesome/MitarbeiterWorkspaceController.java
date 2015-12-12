5cc04a7ef195ad65353dad0bad5a11f45c7b5fc1
rafael <rafael@rafaelkallis.com>
2015-12-12 14:28:27 +0100

    added getUserName()

--- HA6/src/main/java/we/are/awesome/MitarbeiterWorkspaceController.java
+++ HA6/src/main/java/we/are/awesome/MitarbeiterWorkspaceController.java
@@ -3,6 +3,7 @@ package we.are.awesome;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.TypedQuery;
@@ -42,6 +43,11 @@ public class MitarbeiterWorkspaceController extends Controller{
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	public String getUserName(Long userId){
+		
+		return super.getUserEntity(userId).getName();
 	}
 	
 	public List<TaskDAO> getUserTaskList(Long loggedUserId){
